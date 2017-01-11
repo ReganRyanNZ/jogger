5b05ffd05aba56244ca842f8d71a101043f79c03
Regan Ryan <regan.ryan.nz@gmail.com>
2017-01-11 13:05:34 -0300

    get api versioning test working

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module ReganRyan
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,10 +8,16 @@ Rails.application.routes.draw do
   get "manager", to: "pages#manager"
 
 
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :jogs, only: [:create, :show, :update, :destroy]
-      resources :users, only: [:create, :show, :update, :destroy]
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+
+  #   end
+  # end
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+      scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+        resources :jogs, only: [:create, :show, :update, :destroy]
+        resources :users, only: [:create, :show, :update, :destroy]
+      end
     end
-  end
 end
