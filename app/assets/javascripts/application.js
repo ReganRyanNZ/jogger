24b7bc9a14314e1ae34b12b74d19a249117ba3a3
Regan Ryan <regan.ryan.nz@gmail.com>
2017-01-14 13:09:02 -0300

    added datetime picker and new record form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require semantic.min
+//= require calendar.min
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -1,3 +1,34 @@
+var createRecord = function() {
+    var createParams = {
+          "jog[time]": $('#new-jog-time').val(),
+          "jog[distance]": $('#new-jog-distance').val(),
+          "jog[date]": $('#new-record-calendar').calendar('get date'),
+          "jog[user_id]": $('#new-jog-user_id').val()
+        }
+    $ajax({
+        url: "http://api.regan-ryan.dev/jogs",
+        method: 'POST',
+        data: createParams,
+        headers: {
+                    Accept: "application/vnd.regan-ryan.v1",
+                    Authorization: userAuthToken
+                  }
+    }).success(function() {
+        alert("great!");
+        // $editForms.fadeOut(function() {
+        //     clearForm();
+        // });
+        // $boundBox.fadeOut();
+        // var $active = $highlightForm.hasClass('overview') ? $('#' + id + '.overview-btn.overview') : $('.snippet-objects.active');
+        // $active.fadeOut(function() {
+        //     $(this).remove();
+        // });
+    }).error(function() {
+        console.log(arguments);
+        alert('Failed!');
+    });
+};
+
 var deleteRecord = function(id) {
     if (confirm('Are you sure you want to delete this jogging record?')) {
         $ajax({
@@ -24,29 +55,4 @@ var deleteRecord = function(id) {
     }
 
 };
-// var deleteRecord = function(id) {
-//     if (confirm('Are you sure you want to delete this jogging record?')) {
-//         $ajax({
-//             url: "http://api.regan-ryan.dev/jogs/" + id,
-//             method: 'DELETE',
-//             headers: {
-//                         Accept: "application/vnd.regan-ryan.v1",
-//                         Authorization: userAuthToken
-//                       }
-//         }).success(function() {
-//             alert("great!");
-//             // $editForms.fadeOut(function() {
-//             //     clearForm();
-//             // });
-//             // $boundBox.fadeOut();
-//             // var $active = $highlightForm.hasClass('overview') ? $('#' + id + '.overview-btn.overview') : $('.snippet-objects.active');
-//             // $active.fadeOut(function() {
-//             //     $(this).remove();
-//             // });
-//         }).error(function() {
-//             console.log(arguments);
-//             alert('Failed!');
-//         });
-//     }
 
-// };
