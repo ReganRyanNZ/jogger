f150770b4ba846ad939072db79a91c0915ec1649
Regan Ryan <regan.ryan.nz@gmail.com>
2017-01-15 01:59:24 -0300

    add delete function to user manager

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :jogs
+  has_many :jogs, dependent: :destroy
 
   validates :auth_token, uniqueness: true
 
